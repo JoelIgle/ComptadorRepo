eeb05d8094874314d51ad5316d6f63293ae97618
JoelIgle <joeliglesias@iesebre.com>
2023-10-30 20:33:15 +0100

    Primera versió amb el comptador funcionant, falta el timer

--- app/src/main/java/me/joeliglesiassancho/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/joeliglesiassancho/dam/comptador/MainActivity.kt
@@ -1,6 +1,8 @@
 package me.joeliglesiassancho.dam.comptador
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,31 +15,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import me.joeliglesiassancho.dam.comptador.ui.theme.ComptadorTheme
 
 class MainActivity : ComponentActivity() {
+    // Variables
+    internal lateinit var tapMeButton : Button
+    internal lateinit var timeTextView : TextView
+    internal lateinit var counterTextView : TextView
+    // Inicialitzem comptador i el temps
+    internal var counter = 0
+    internal var time = 60
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ComptadorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Joel")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+//      Aixó és per a mostrar el comptador de clicks
+
+        tapMeButton.setOnClickListener{
+            incrementCounter()
         }
+//        Aixó és per a mostrar el temps disponible
+          timeTextView.text = getString(R.string.timeText, time)
+    }
+
+//    Aquesta funció fa que s'incremente el comptador a cada click
+    private fun incrementCounter() {
+        counter += 1
+        counterTextView.text = counter.toString()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComptadorTheme {
-        Greeting("Android")
-    }
-}
